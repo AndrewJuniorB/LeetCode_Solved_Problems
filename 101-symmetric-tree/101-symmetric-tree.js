419d75001dd9b5cf7c73439b3e7572c2c1c19d85
Andrew Burlakov <andrew.burlakov@outlook.com>
2022-01-20 00:08:04 -0500

    Time: 124 ms (31.00%), Space: 40.6 MB (82.00%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+if (!root) return true;
+    function mirror(a,b) {
+        if (!a && !b) return true;
+        if (!a || !b || a.val !== b.val) return false;
+        return mirror(a.right, b.left) && mirror(a.left, b.right);
+    }
+return mirror(root.left, root.right);
+};
