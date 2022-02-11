6944be0c80b1085c1000ad168e487a73acd0764c
Andrew Burlakov <andrew.burlakov@outlook.com>
2022-02-10 20:44:52 -0800

    Time: 1231 ms (5.02%), Space: 43.2 MB (55.48%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -3,6 +3,5 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let result = nums.filter((num) => nums.indexOf(num) === nums.lastIndexOf(num))
-   return result;
+    return nums.filter((num) => nums.indexOf(num) === nums.lastIndexOf(num))
 };
